d081449e4841bbf79044623c86c2ca588268a85f
acham1 <alanchamis@gmail.com>
2016-05-03 18:41:28 -0400

    tweaked ssdcost

--- project2/SSDCost.m
+++ project2/SSDCost.m
@@ -1,12 +1,12 @@
-%Compute the SSD cost of the intensity between two images to evulate the
+%Compute the SSD cost of the intensity between two images to evaluate the
 %registration process
 %Input: matrices of image 1 and image 2 
-% Output: SSD cost value
+%Output: SSD cost value
 
 function [cost] = SSDCost(img1,img2)
 
 I_final = img2 - img1;
 cost = sum(sum(sum(I_final.^2)));
-cost = sqrt(cost);
+cost = sqrt(cost) / numel(img1);
 
 end
