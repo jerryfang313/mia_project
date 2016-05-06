6a48ecc6551f4c8314ba03cc91bfa4e8821aa4a0
acham1 <alanchamis@gmail.com>
2016-05-06 00:42:29 -0400

    added description to mcDetect

--- project1/mcDetect.m
+++ project1/mcDetect.m
@@ -1,3 +1,5 @@
+% newInputImage is the matrix obtained by calling the function imread on the new test
+% image path
 function [ x, y, r, labelmask ] = mcDetect( newInputImage )
 
 [x, y, r, labelmask] = returnCalcification(newInputImage);
